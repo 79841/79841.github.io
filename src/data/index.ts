05b7f73e32bcbd5219e8fef6a3d36b9f02c4be29
LEtMeDEv <79841@naver.com>
2024-01-15 23:37:58 +0900

    chore: data

---
+++ src/data/index.ts
@@ -0,0 +1,5 @@
+export * from "./siteMetadata";
+export * from "./navigationLinks";
+export * from "./projectsData";
+export * from "./skillsData";
+export * from "./experiencesData";
---
+++ src/data/navigationLinks.ts
@@ -0,0 +1,7 @@
+export const navigationLinks = {
+  home: { name: "Home", path: "/" },
+  profile: { name: "Profile", path: "/#profile" },
+  skill: { name: "Skill", path: "/#skill" },
+  experience: { name: "Experience", path: "/#experience" },
+  project: { name: "Project", path: "/#project" },
+};
