c2f9320422297e4f8fa17c2cfc819d412db26543
LEtMeDEv <79841@naver.com>
2024-01-16 22:59:11 +0900

    chore: navigation links

--- src/data/navigationLinks.ts
+++ src/data/navigationLinks.ts
@@ -1,7 +1,7 @@
 export const navigationLinks = {
-  home: { name: "Home", path: "/" },
-  profile: { name: "Profile", path: "/#profile" },
+  home: { name: "Home", path: "/#home" },
   skill: { name: "Skill", path: "/#skill" },
   experience: { name: "Experience", path: "/#experience" },
   project: { name: "Project", path: "/#project" },
+  contact: { name: "Contact", path: "/#Contact" },
 };
