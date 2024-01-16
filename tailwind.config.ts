a13266edc92bf4d722a34eb179b79d10843133ae
LEtMeDEv <79841@naver.com>
2024-01-16 22:48:10 +0900

    chore: add animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        "show-up": {
+          from: { width: "5rem", height: "5rem" },
+          to: { width: "15rem", height: "15rem" },
+        },
+        rising: {
+          from: { opacity: "0", transform: "translateY(20px)" },
+          to: { opacity: "1", tranform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "show-up": "1s ease-in-out 1 forwards show-up",
+        rising: "1s ease-in-out 1 forwards rising",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
