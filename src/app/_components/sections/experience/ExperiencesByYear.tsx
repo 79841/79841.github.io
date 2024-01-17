3dec314c393c2ad9378fd7378dba9b88a31513ad
LEtMeDEv <79841@naver.com>
2024-01-17 23:22:55 +0900

    feat: change font weight

--- src/app/_components/sections/experience/ExperiencesByYear.tsx
+++ src/app/_components/sections/experience/ExperiencesByYear.tsx
@@ -11,8 +11,10 @@ export const ExperiencesByYear = ({
   experiences,
 }: TExperiencesByYear) => {
   return (
-    <div className="flex border gap-4 p-4">
-      <div className="w-24 flex items-center justify-center">{year}</div>
+    <div className="flex gap-4 p-4">
+      <div className="flex w-24 items-center justify-center font-bold">
+        {year}
+      </div>
       <div className="flex flex-col gap-2">
         {experiences.map((experience) => (
           <Experience key={experience.title} experience={experience} />
