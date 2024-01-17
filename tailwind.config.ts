a4ea5c3d0598f96f7ebe31ff502e8759d0081f72
LEtMeDEv <79841@naver.com>
2024-01-17 23:21:25 +0900

    chore: indicator animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,13 +19,44 @@ const config: Config = {
           to: { width: "15rem", height: "15rem" },
         },
         rising: {
-          from: { opacity: "0", transform: "translateY(20px)" },
+          from: { opacity: "0", transform: "translateY(35px)" },
           to: { opacity: "1", tranform: "translateY(0)" },
+        },
+        "indicator-show-up": {
+          "0%": {
+            width: "0rem",
+            height: "0rem",
+          },
+          "80%": {
+            width: "3.6rem",
+            height: "3.5rem",
+          },
+          "100%": {
+            width: "10rem",
+            height: "3.5rem",
+          },
+        },
+        "indicator-disappear": {
+          "0%": {
+            width: "10rem",
+            height: "3.5rem",
+          },
+          "20%": {
+            width: "3.6rem",
+            height: "3.5rem",
+          },
+          "100%": {
+            width: "0rem",
+            height: "0rem",
+          },
         },
       },
       animation: {
         "show-up": "1s ease-in-out 1 forwards show-up",
-        rising: "1s ease-in-out 1 forwards rising",
+        rising: "1.5s ease-in-out 1 forwards rising",
+        "indicator-show-up": "1.5s ease-in-out 1 forwards indicator-show-up",
+        "indicator-disappear":
+          "1.5s ease-in-out 1 forwards indicator-disappear",
       },
     },
   },
