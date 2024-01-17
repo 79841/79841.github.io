b89b06f64b94a01b66c07987b481655c7ed45f23
LEtMeDEv <79841@naver.com>
2024-01-17 23:22:23 +0900

    feat: experience section, project section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { IntroSection } from "./_components/sections/intro";
 // import { ContactSection } from "./_components/sections/contact";
-// import { ExperienceSection } from "./_components/sections/experience";
-// import { ProjectSection } from "./_components/sections/project";
+import { ExperienceSection } from "./_components/sections/experience";
+import { ProjectSection } from "./_components/sections/project";
 import { SkillSection } from "./_components/sections/skill";
 
 export default function Home() {
@@ -9,9 +9,9 @@ export default function Home() {
     <main className="flex w-full flex-col items-center justify-center">
       <IntroSection />
       <SkillSection />
-      {/* <ExperienceSection />
+      <ExperienceSection />
       <ProjectSection />
-      <ContactSection /> */}
+      {/* <ContactSection /> */}
     </main>
   );
 }
