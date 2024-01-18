f60cde5c21d288bce26f4c2cb88d737224ef3b03
LEtMeDEv <79841@naver.com>
2024-01-18 20:42:56 +0900

    design: change layout

--- src/app/_components/sections/project/ProjectSection.tsx
+++ src/app/_components/sections/project/ProjectSection.tsx
@@ -7,14 +7,12 @@ import { SectionLayout } from "@/components/ui/layout/section";
 export const ProjectSection = () => {
   return (
     <SectionLayout id={"project"} nextSection={navigationLinks.contact}>
-      <div className="mb-48">
+      <div className="mb-24">
         <SectionTitle title="Project" />
       </div>
-      <div className="flex flex-col items-center gap-24">
+      <div className="flex flex-col items-center gap-24 pb-32">
         {Object.values(projectsData).map((project) => (
-          // <ScrollRisingBox key={project.name}>
           <Project key={project.name} project={project} />
-          // </ScrollRisingBox>
         ))}
       </div>
     </SectionLayout>
