3e752525d01beecbdfefd07fe9ce0d4b9e58a377
LEtMeDEv <79841@naver.com>
2024-01-19 19:46:41 +0900

    feat: footer

--- src/app/_components/footer/Footer.tsx
+++ src/app/_components/footer/Footer.tsx
@@ -1,5 +1,26 @@
-import React from "react";
-
+import { ImBlogger } from "react-icons/im";
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
+import { siteMetadata } from "@/data";
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex h-36 flex-col items-center justify-center gap-4 text-zinc-300">
+      <div className="flex items-center justify-center gap-4 text-xl ">
+        <Link
+          href={siteMetadata.githubUrl}
+          className="flex duration-500 hover:text-black"
+        >
+          <FaGithub />
+        </Link>
+        <div className="h-full border"></div>
+        <Link
+          href={siteMetadata.blogUrl}
+          className="flex duration-500 hover:text-blue-600"
+        >
+          <ImBlogger />
+        </Link>
+      </div>
+      <div>© 2024. In Ji Myeong. All rights reserved.</div>
+    </div>
+  );
 };
