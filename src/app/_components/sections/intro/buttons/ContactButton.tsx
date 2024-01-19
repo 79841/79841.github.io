12a2b1b5de1d32f27223224e8950fbcfc3301a22
LEtMeDEv <79841@naver.com>
2024-01-19 16:30:02 +0900

    feat: update intro buttons

--- src/app/_components/sections/intro/buttons/ContactButton.tsx
+++ src/app/_components/sections/intro/buttons/ContactButton.tsx
@@ -1,11 +1,16 @@
+import { navigationLinks } from "@/data";
+import Link from "next/link";
 import React from "react";
 import { FaArrowAltCircleRight } from "react-icons/fa";
 
 export const ContactButton = () => {
   return (
-    <button className="flex w-fit items-center justify-center gap-2 rounded-full bg-zinc-100 px-6 py-3">
+    <Link
+      href={navigationLinks.contact.path}
+      className="flex w-fit items-center justify-center gap-2 rounded-full bg-zinc-100 px-6 py-3"
+    >
       <div>Contact</div>
       <FaArrowAltCircleRight size={18} />
-    </button>
+    </Link>
   );
 };
--- src/app/_components/sections/intro/buttons/ResumeButton.tsx
+++ src/app/_components/sections/intro/buttons/ResumeButton.tsx
@@ -1,11 +1,18 @@
+import { siteMetadata } from "@/data";
+import Link from "next/link";
 import React from "react";
 import { IoMdDownload } from "react-icons/io";
 
 export const ResumeButton = () => {
   return (
-    <button className="flex w-fit items-center justify-center gap-2 rounded-full border bg-blue-600 px-6 py-3 text-white">
+    <Link
+      href={siteMetadata.resumePath}
+      download={siteMetadata.resumePath.split("/").at(-1)}
+      target="_blank"
+      className="flex w-fit items-center justify-center gap-2 rounded-full border bg-blue-600 px-6 py-3 text-white"
+    >
       <div>Resume</div>
       <IoMdDownload size={18} />
-    </button>
+    </Link>
   );
 };
