aad6daf0b24f4e633574f7b69f591623e881844a
LEtMeDEv <79841@naver.com>
2024-01-20 21:15:08 +0900

    design: mobile layout updated

--- src/components/ui/layout/section/SectionLayout.tsx
+++ src/components/ui/layout/section/SectionLayout.tsx
@@ -21,9 +21,9 @@ export const SectionLayout = ({
       ref={ref}
       className="relative mb-32 flex min-h-screen w-full flex-col items-center justify-center gap-8"
     >
-      <div className="mb-auto h-16"></div>
+      <div className="mb-auto sm:h-16"></div>
       {children}
-      <div className="sticky bottom-12 mt-auto min-h-16 -translate-y-[2rem]">
+      <div className="sticky bottom-4 mt-auto min-h-16 -translate-y-[2rem] sm:bottom-12">
         {inView && <Indicator nextSection={nextSection} />}
       </div>
     </section>
