8e5de606a25f9db9dd9ef5f7edb54b130228f772
LEtMeDEv <79841@naver.com>
2025-03-13 23:05:14 +0900

    design: 경험 섹션의 레이아웃 및 스타일 개선

--- src/app/_components/sections/experience/Experience.tsx
+++ src/app/_components/sections/experience/Experience.tsx
@@ -6,7 +6,9 @@ type TExperienceProps = {
 export const Experience = ({ experience }: TExperienceProps) => {
   return (
     <div className="flex flex-col gap-1">
-      <div>{experience.title}</div>
+      <div className="overflow-hidden whitespace-normal break-keep">
+        {experience.title}
+      </div>
       <div className="text-xs text-zinc-600">
         {experience.content?.map((content) => (
           <div key={content}>{content}</div>
--- src/app/_components/sections/experience/ExperiencesByYear.tsx
+++ src/app/_components/sections/experience/ExperiencesByYear.tsx
@@ -10,11 +10,11 @@ export const ExperiencesByYear = ({
   experiences,
 }: TExperiencesByYear) => {
   return (
-    <div className="flex gap-4 p-4">
-      <div className="flex w-24 items-center justify-center font-bold">
+    <div className="flex gap-5 pl-4 sm:p-4">
+      <div className="flex w-12 min-w-12 items-center justify-start font-bold sm:w-24 sm:justify-center">
         {year}
       </div>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-1 flex-col gap-4">
         {experiences.map((experience) => (
           <Experience key={experience.title} experience={experience} />
         ))}
