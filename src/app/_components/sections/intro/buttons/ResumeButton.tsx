4ea6f5bc47882b9e66a11bceaaf1c69af39eb4f6
LEtMeDEv <79841@naver.com>
2025-06-26 22:14:55 +0900

    chore: 이력서 PDF 타입 추가 및 다운로드 링크 수정

--- src/app/_components/sections/intro/buttons/ResumeButton.tsx
+++ src/app/_components/sections/intro/buttons/ResumeButton.tsx
@@ -6,6 +6,7 @@ export const ResumeButton = () => {
   return (
     <Link
       href={siteMetadata.resumePath}
+      type="application/pdf"
       download={siteMetadata.resumePath.split("/").at(-1)}
       target="_blank"
       className="flex h-14 w-fit items-center justify-center gap-2 rounded-full border bg-blue-600 px-7 text-white"
